e9a399407fea02e5aca85bf55c4c93eb0e29b035
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-03-18 21:54:47 +0530

    Create New Native project
    Failed to implement native app with expo cli.
    Failed to run project in emulator.
    Delete project with expo cli and Create new project with React Native
    CLI.

--- App.js
+++ App.js
@@ -1,39 +1,112 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
 import {
-  RecyclerViewBackedScrollView,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
+  useColorScheme,
   View,
-} from "react-native";
-import AppNavigator from "./src/navigations/Navigator";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
+} from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    isFontLoaded: false,
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      fontOne: require("./src/fonts/Simvoni-Italic.otf"),
-      fontTwo: require("./src/fonts/Simvoni-BoldItalic.otf"),
-      fontThree: require("./src/fonts/Macho Weak.ttf"),
-    });
-    this.setState({ isFontLoaded: true });
-  }
-
-  render() {
-    return this.state.isFontLoaded === true ? <AppNavigator /> : AppLoading;
-  }
-}
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
+
+export default App;
