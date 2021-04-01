065b0261b6956148d9536e2c3253f34f75762ba4
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-04-01 21:21:30 +0530

    Add Strip add view bar to dashboard background.

---
+++ src/Components/StripAdView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box} from "@material-ui/core";
+
+const StripAdView = () => {
+    return (
+        <Box>
+            <img style={{height:"100px", width: "100%"}}/>
+        </Box>
+    )
+}
+
+export default StripAdView
