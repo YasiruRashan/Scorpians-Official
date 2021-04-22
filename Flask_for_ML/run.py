10b44ea0012d8978057095a299e1fc7cac19e1df
sasmitha134 <66547536+sasmitha134@users.noreply.github.com>
2021-04-22 21:46:02 +0530

    Update Flask_for_ML folder with changes.

--- Flask_for_ML/run.py
+++ Flask_for_ML/run.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template , request
+from flask import Flask, jsonify, request
+import json
 import pickle
+
+
+from numpy.core.arrayprint import printoptions
+
+import numpy as np
+import pickle
+from flask_cors import CORS
+from flask import jsonify
+from flask import request,json
+
+def load_models():
+    file_name = "F:\Flask_for_ML\model_file.p"
+    with open(file_name, 'rb') as pickled:
+        data = pickle.load(pickled)
+        model = data['model']
+    return model
 
 app = Flask(__name__)
 
 
-@app.route('/', methods =['GET','POST'])
-def home():
-    if request.method== 'POST':
-        model = pickle.load(open('price_pred_model.pkl','rb'))
-        user_input = request.form.get('size')
-        print(user_input , type(user_input))
-        user_input = float(user_input)
-        prediction = model.predict([[user_input]])
-        print(prediction)
-    return render_template('index.html')
+@app.route('/predict', methods=['GET'])
+def predict():
+    # stub input features
+    # request_json = request.get_json()
+    # x = request_json['input']
+
+    ata_in=[1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    
+    x_in =np.array(ata_in).reshape(1,-1)
+    # load model
+    model = load_models()
+    prediction = model.predict(x_in)[0]
+    # response = json.dumps({'response': prediction})
+    response = json.dumps([{"id": 1,
+    "response": prediction}])
+    
+    return response
 
 
 if __name__ =='__main__':
